78c3c6aea002bd1de153ea5f26040a68c4e14990
Andrei <andrei.kolesnik@gmail.com>
2021-12-24 20:15:43 -0800

    Day 2 / part 2

--- 02/02.py
+++ 02/02.py
@@ -23,6 +23,25 @@ def day_02_part_1(data: List[str]) -> int:
     return x * y
 
 
+def day_02_part_2(data: List[str]) -> int:
+    x = 0
+    y = 0
+    aim = 0
+
+    for instruction in data:
+        words = instruction.split()
+        direction = words[0]
+        distance = int(words[1])
+        if direction == 'forward':
+            x += distance
+            y += aim * distance
+        elif direction == 'down':
+            aim += distance
+        elif direction == 'up':
+            aim -= distance
+
+    return x * y
+
 if __name__ == '__main__':
     with open('02-test.txt', 'r') as test_data_file:
         with open('02.txt', 'r') as real_data_file:
@@ -31,3 +50,6 @@ if __name__ == '__main__':
 
             assert_result(150, day_02_part_1(data=test_data))
             assert_result(1728414, day_02_part_1(data=real_data))
+
+            assert_result(900, day_02_part_2(data=test_data))
+            assert_result(1765720035, day_02_part_2(data=real_data))
