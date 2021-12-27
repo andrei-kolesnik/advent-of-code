95c0f4d93670f2e564bee20e46a59fcd4fca7817
Andrei <andrei.kolesnik@gmail.com>
2021-12-27 11:59:09 -0800

    Day 3 / part 2

--- 03/03.py
+++ 03/03.py
@@ -1,4 +1,5 @@
 from typing import List
+from operator import lt, gt
 
 
 def assert_result(expected, actual):
@@ -26,6 +27,36 @@ def day_03_part_1(data: List[str]) -> int:
     return gamma * epsilon
 
 
+def day_03_part_2_filter(data: List[str], position: int, operator, tie_breaker: int) -> List[str]:
+    part_one = []
+    part_zero = []
+    for number in data:
+        if int(number[position]):
+            part_one.append(number)
+        else:
+            part_zero.append(number)
+    if operator(len(part_zero), len(part_one)):
+        return part_zero
+    elif operator(len(part_one), len(part_zero)):
+        return part_one
+    return part_one if tie_breaker else part_zero
+
+
+def day_03_part_2_for_digit(data: List[str], digit: int) -> int:
+    for position in range(len(data[0])):
+        data = day_03_part_2_filter(data=data, position=position, operator=gt if digit else lt, tie_breaker=digit)
+        if len(data) == 1:
+            return int(data[0], 2)
+    print("ERROR in data: more than one answer")
+    return 0
+
+
+def day_03_part_2(data: List[str]) -> int:
+    oxygen_generator_rating = day_03_part_2_for_digit(data=data, digit=1)
+    co2_scrubber_rating = day_03_part_2_for_digit(data=data, digit=0)
+    return oxygen_generator_rating * co2_scrubber_rating
+
+
 if __name__ == '__main__':
     with open('03-test.txt', 'r') as test_data_file:
         with open('03.txt', 'r') as real_data_file:
@@ -34,3 +65,7 @@ if __name__ == '__main__':
 
             assert_result(198, day_03_part_1(data=test_data))
             assert_result(3969000, day_03_part_1(data=real_data))
+
+            assert_result(230, day_03_part_2(data=test_data))
+            assert_result(4267809, day_03_part_2(data=real_data))
+
